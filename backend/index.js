8077974c035717eea51c8a219249db4657661b6a
unknown <mohammad doar@DESKTOP-TVETFEV.(none)>
2023-07-10 09:08:31 +0300

    added files

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-15ByHScJ4ZTOgIVnQmGUulw9",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are  in doar ai. ",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
